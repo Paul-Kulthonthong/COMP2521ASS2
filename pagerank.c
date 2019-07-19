6851fd4f020477caba6deb4a2fb061091c431e60
Paul-Kulthonthong <z5208916@student.unsw.edu.au>
2019-07-19 14:29:15 +1000

    graph created and ready

--- pagerank.c
+++ pagerank.c
@@ -5,24 +5,38 @@
 #include <ctype.h>
 #include <libgen.h>
 #include <math.h>
+#include <err.h>
+#include <stdbool.h>
+#include <sysexits.h>
 
 #include "pagerank.h"
 
 #define MAX 100
 
 void PageRankW(float d, float diffPR, float maxIterations){
-    FileList returnedfilelist = getcollectionfilelist();
+    FileList filelistofurl = getcollectionfilelist();
+    int num_of_files_in_collection = getnumofiles(filelistofurl);
 
-    while(returnedfilelist != NULL){
-        printf("[%s]\n", returnedfilelist->filename);
-        returnedfilelist = returnedfilelist->next;
-    }
+    printf("{%d}\n", num_of_files_in_collection);
+
+    Graph urlgraph = getcollectiongraph(filelistofurl, num_of_files_in_collection);
+    showGraph(urlgraph, 1);
+
+
+    //printf("%d is part of vertex %s %s\n", urlgraph->edges[0][1], urlgraph->vertex[0], urlgraph->vertex[1]);
+
+//    List_Urls_PageRanks= calculatePageRank(g, d, diffPR, maxIterations);
+
+//    Ordered_List_Urls_PageRanks= order (List_Urls_PageRanks)
+//    Output Ordered_List_Urls_PageRanksto “pagerankList.txt”
+
 
 
     return;
 }
 
-
+//THIS IS FOR CREATING THE LINK LIST OF FILES TO BE OPENED
+//------------------------------------------------------------------------------
 FileList getcollectionfilelist(){
     FILE * collectionfile;
     char filestemp[MAX];
@@ -71,6 +85,211 @@ FileList addtofilelist(FileList addonto, char *inputfile){
     }
     return addonto;
 }
+
+int getnumofiles(FileList filelist){
+    int num_of_files_in_collection = 0;
+    FileList curr = filelist;
+    while(curr != NULL){
+        curr = curr->next;
+        num_of_files_in_collection++;
+    }
+    return num_of_files_in_collection;
+}
+//------------------------------------------------------------------------------
+
+
+//THIS IS FOR CREATING THE GRAPH OF FILES OPENED
+//------------------------------------------------------------------------------
+Graph getcollectiongraph(FileList file_list, int num_of_files){
+
+    Graph newgraph = newGraph((size_t)num_of_files);
+    FILE *f;
+    char filestemp[MAX];
+    char infileword[MAX];
+
+    FileList curr = file_list;
+    while(curr != NULL){
+        if ((f = fopen(curr->filename,"r")) == NULL) {
+            fprintf(stderr, "Can't open file %s\n", curr->filename);
+            return NULL;
+    	}
+
+        rewind(f);
+        int redflag = 2;
+        while(fscanf(f, "%s", infileword) != EOF && redflag>0){
+            if((strcmp(infileword, "#end") == 0) || (strcmp(infileword, "#start") == 0)){
+                //NOTHING
+            }
+            else if(strcmp(infileword, "Section-1") == 0){
+                redflag--;
+            }
+            else{
+                if(strcmp(curr->filename, getfiledir(infileword)) != 0 ){
+                    addEdge(newgraph, curr->filename, getfiledir(infileword));
+                }
+            }
+        }
+        fclose(f);
+        curr = curr->next;
+    }
+    return newgraph;
+}
+
+Graph newGraph (size_t maxV){
+	Graph new = malloc (sizeof *new);
+	if (new == NULL)
+		err (EX_OSERR, "couldn't allocate Graph");
+	*new = (GraphRep){.nV = 0, .maxV = maxV};
+
+	new->vertex = calloc (maxV, sizeof (char *));
+	if (new->vertex == NULL)
+		err (EX_OSERR, "calloc");
+	new->edges = calloc (maxV, sizeof (Num *));
+	if (new->edges == NULL)
+		err (EX_OSERR, "calloc");
+
+	for (size_t i = 0; i < maxV; i++) {
+		new->edges[i] = calloc (maxV, sizeof (Num));
+		if (new->edges[i] == NULL)
+			err (EX_OSERR, "calloc");
+
+		for (size_t j = 0; j < maxV; j++)
+			new->edges[i][j] = 0;
+	}
+	return new;
+}
+
+
+
+// addEdge(Graph,Src,Dest)
+// - add an edge from Src to Dest
+// - returns 1 if edge successfully added
+// - returns 0 if unable to add edge
+//   (usually because nV exceeds maxV)
+bool addEdge (Graph g, char *src, char *dest){
+	assert (g != NULL);
+
+	ssize_t v = vertexID (src, g->vertex, g->nV);
+	if (v < 0) {
+		if (g->nV >= g->maxV)
+			return false;
+		v = (ssize_t) addVertex (src, g->vertex, g->nV);
+		g->nV++;
+	}
+
+	ssize_t w = vertexID (dest, g->vertex, g->nV);
+	if (w < 0) {
+		if (g->nV >= g->maxV)
+			return false;
+		w = (ssize_t) addVertex (dest, g->vertex, g->nV);
+		g->nV++;
+	}
+	g->edges[v][w] = 1;
+
+	return true;
+}
+
+// isConnected(Graph,Src,Dest)
+// - check whether there is an edge from Src->Dest
+bool isConnected (Graph g, char *src, char *dest){
+	assert (g != NULL);
+	ssize_t v = vertexID (src, g->vertex, g->nV);
+	ssize_t w = vertexID (dest, g->vertex, g->nV);
+	if (v < 0 || w < 0)
+		return 0;
+	return g->edges[v][w] == 1;
+}
+
+// nVertices(Graph)
+// - return # vertices currently in Graph
+size_t nVertices (Graph g){
+	assert (g != NULL);
+	return g->nV;
+}
+
+// showGraph(Graph)
+// - display Graph
+void showGraph (Graph g, int mode){
+	assert (g != NULL);
+	if (g->nV == 0) {
+		puts ("Graph is empty");
+		return;
+	}
+
+	printf ("Graph has %zu vertices:\n", g->nV);
+	if (mode == 1) {
+		for (size_t i = 0; i < g->nV; i++) {
+			for (size_t j = 0; j < g->nV; j++)
+				printf ("%d ", g->edges[i][j]);
+			putchar ('\n');
+		}
+	} else {
+		for (size_t i = 0; i < g->nV; i++) {
+			printf ("Vertex: %s\n", g->vertex[i]);
+			printf ("connects to\n");
+			for (size_t j = 0; j < g->nV; j++) {
+				if (g->edges[i][j])
+					printf ("   %s\n", g->vertex[j]);
+			}
+		}
+	}
+}
+
+// Helper functions
+
+// vertexID(Str,Names,N)
+// - searches for Str in array of Names[N]
+// - returns index of Str if found, -1 if not
+ssize_t vertexID (char *str, char **names, size_t N){
+	for (size_t i = 0; i < N; i++)
+		if (strEQ (str, names[i]))
+			return (ssize_t) i;
+	return -1;
+}
+
+// addVertex(Str,Names,N)
+// - add Str at end of Names
+size_t addVertex (char *str, char **names, size_t N){
+	names[N] = strdup (str);
+	return N;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// dropGraph(Graph)
+// - clean up memory associated with Graph
+void dropGraph (Graph g){
+	if (g == NULL)
+		return;
+
+	for (size_t i = 0; i < g->nV; i++)
+		free (g->vertex[i]);
+	for (size_t i = 0; i < g->maxV; i++)
+		free (g->edges[i]);
+
+	free (g->vertex);
+	free (g->edges);
+	free (g);
+}
+
+
+
 
 /*void gatherwebpages(char *collectionFilename){
     FILE *f;
--- pagerank.h
+++ pagerank.h
@@ -1,16 +1,44 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+
+#define strEQ(g, t) (strcmp ((g), (t)) == 0)
+typedef unsigned char Num;
+
 struct FileListNode {
     char *filename;
 	struct FileListNode *next;
 };
 typedef struct FileListNode *FileList;
 
+typedef struct GraphRep {
+	size_t nV;
+	size_t maxV;
+	char **vertex;
+	Num **edges;
+} GraphRep;
+typedef struct GraphRep *Graph;
 
-void gatherwebpages(char *collectionFilename);
-char * getfiledir(char *collectionfilename);
+
+
 void PageRankW(float d, float diffPR, float maxIterations);
 
 
-
+char * getfiledir(char *collectionfilename);
 FileList getcollectionfilelist();
 FileList newFileNode(char *inputfile);
 FileList addtofilelist(FileList addonto, char *inputfile);
+int getnumofiles(FileList filelist);
+
+
+Graph getcollectiongraph(FileList file_list, int num_of_files);
+
+
+Graph newGraph (size_t maxV);
+void dropGraph (Graph g);
+bool addEdge (Graph g, char *src, char *dest);
+bool isConnected (Graph g, char *src, char *dest);
+size_t nVertices (Graph g);
+void showGraph (Graph g, int mode);
+ssize_t vertexID (char *str, char **names, size_t N);
+size_t addVertex (char *, char **, size_t);
